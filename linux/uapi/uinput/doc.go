221b20fa44093d910cf531b61d94356a63aaf331
andrieee44 <andrieee44@gmail.com>
2025-09-03 14:37:12 +0800

    uapi/uinput: Update package level documentation

--- linux/uapi/uinput/doc.go
+++ linux/uapi/uinput/doc.go
@@ -2,7 +2,4 @@
 // allowing user-space programs to create and manage virtual input devices.
 // It defines constants and functions for constructing ioctl request codes
 // used to configure devices, enable event types, and send input events.
-// The package mirrors the uinput and evdev ioctl interfaces, enabling
-// fine-grained control over virtual keyboards, mice, gamepads, and other
-// input devices from Go.
 package uinput
